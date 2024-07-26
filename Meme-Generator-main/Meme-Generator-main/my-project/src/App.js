ee773c0c17e03e10c0e8fee43de117ed6d3c9c54
IITI-tushar <165766280+IITI-tushar@users.noreply.github.com>
2024-07-26 17:30:43 +0530

    Update App.js__tushar

--- Meme-Generator-main/Meme-Generator-main/my-project/src/App.js
+++ Meme-Generator-main/Meme-Generator-main/my-project/src/App.js
@@ -1,17 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-import Homepage from "./Components/Homepage"
-import Loading from "./Components/Loading";
-import Game from "./Components/Game";
-// import Game from "./Components/Check";
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './Components/Homepage';
+import Loading from './Components/Loading';
+import Game from './Components/Game';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
+import ProtectedRoute from './Components/ProtectedRoutes'; // Import ProtectedRoute
+import { AuthProvider } from './AuthContext'; // Import AuthProvider
 
 export default function App() {
   return (
-    <div>
+    <AuthProvider>
+      <div>
         <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/loading" element={<Loading />} />
-          <Route path="/game" element={<Game />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/loading" element={<ProtectedRoute element={Loading} />} />
+          <Route path="/game" element={<ProtectedRoute element={Game} />} />
+          <Route path="/homepage" element={<ProtectedRoute element={Homepage} />} />
         </Routes>
       </div>
-  )
+    </AuthProvider>
+  );
 }
